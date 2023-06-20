2c891efecd9b6587c5d1069916f39511e818b319
FANT0M100 <giorgie103188@gmail.com>
2023-06-20 21:48:12 +0400

    end styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Input from "./components/Input,";
 function App() {
   return (
     <div className="sign-up">
+      <h1>Sign Up</h1>
       <form>
         <Input
           type="text"
@@ -11,6 +12,35 @@ function App() {
           label="Username"
           placeholder="Enter Username"
         />
+
+        <Input
+          type="text"
+          id="phonenumber"
+          label="Phone Number"
+          placeholder="Enter Phone Number"
+        />
+
+        <Input
+          type="email"
+          id="email"
+          label="Email"
+          placeholder="Enter Email"
+        />
+
+        <Input
+          type="password"
+          id="password"
+          label="Password"
+          placeholder="Enter Password"
+        />
+
+        <Input
+          type="password"
+          id="confirmpassword"
+          label="Confirm Password"
+          placeholder="Enter Confirm Password"
+        />
+        <button>Sign Up</button>
       </form>
     </div>
   );
