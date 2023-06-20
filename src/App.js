ada63a55761c4f7049943badbb424052bac88aaf
FANT0M100 <giorgie103188@gmail.com>
2023-06-20 22:34:18 +0400

    end form validation

--- src/App.js
+++ src/App.js
@@ -4,12 +4,35 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import Input from "./components/Input,";
 
-const schema = yup.object({});
+const schema = yup.object({
+  username: yup.string().required("Username is a required field"),
+  phonenumber: yup
+    .string()
+    .required("Phone Number is a required field")
+    .matches(
+      /^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{3}$/,
+      "Phone number is not valid"
+    ),
+  email: yup
+    .string()
+    .required("Email is a required field")
+    .email("Email is not valid"),
+  password: yup.string().min(6, "Password must be at 6 characters"),
+  confirmpassword: yup
+    .string()
+    .oneOf([yup.ref("password")], "Password must be match"),
+});
 
 function App() {
-  const { handleSubmit, register } = useForm({
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
     resolver: yupResolver(schema),
   });
+
+  console.log(errors);
 
   const formSubmit = (data) => {
     console.log(data);
@@ -25,6 +48,7 @@ function App() {
           label="Username"
           placeholder="Enter Username"
           register={{ ...register("username") }}
+          errorMessage={errors.username?.message}
         />
 
         <Input
@@ -33,6 +57,7 @@ function App() {
           label="Phone Number"
           placeholder="Enter Phone Number"
           register={{ ...register("phonenumber") }}
+          errorMessage={errors.phonenumber?.message}
         />
 
         <Input
@@ -41,6 +66,7 @@ function App() {
           label="Email"
           placeholder="Enter Email"
           register={{ ...register("email") }}
+          errorMessage={errors.email?.message}
         />
 
         <Input
@@ -49,6 +75,7 @@ function App() {
           label="Password"
           placeholder="Enter Password"
           register={{ ...register("password") }}
+          errorMessage={errors.password?.message}
         />
 
         <Input
@@ -57,6 +84,7 @@ function App() {
           label="Confirm Password"
           placeholder="Enter Confirm Password"
           register={{ ...register("confirmpassword") }}
+          errorMessage={errors.confirmpassword?.message}
         />
         <button>Sign Up</button>
       </form>
--- src/components/Input,.js
+++ src/components/Input,.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-const Input = ({ id, label, placeholder, type, register }) => {
+const Input = ({ id, label, placeholder, type, register, errorMessage }) => {
   return (
     <div className="input-group">
       <label htmlFor={id}>{label}</label>
       <input type={type} id={id} placeholder={placeholder} {...register} />
+      <span className="error-message">{errorMessage}</span>
     </div>
   );
 };
